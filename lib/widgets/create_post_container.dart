346b99ce9b52b3b77fb14deac406fc2e17460f36
OgheneMichael <zonalmeet@gmail.com>
2020-08-10 23:01:16 +0100

    made create post container card in desktop mode

--- lib/widgets/create_post_container.dart
+++ lib/widgets/create_post_container.dart
@@ -12,64 +12,72 @@ class CreatePostContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 2.0),
-      color: Colors.white,
-      child: Column(
-        children: [
-          Row(
-            children: [
-              ProfileAvatar(
-                imageUrl: currentUser.imageUrl,
-              ),
-              const SizedBox(
-                width: 8.0,
-              ),
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration.collapsed(
-                    hintText: 'What\'s on your mind?',
-                  ),
-                ),
-              ),
-            ],
-          ),
-          const Divider(height: 10.0, thickness: .5),
-          Container(
-            height: 40.0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    final bool isDesktop = Responsive.isDesktop(context);
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: isDesktop ? 5.0 : 0.0),
+      elevation: isDesktop ? 1.0 : 0.0,
+      shape: isDesktop
+          ? RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0))
+          : null,
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 2.0),
+        color: Colors.white,
+        child: Column(
+          children: [
+            Row(
               children: [
-                FlatButton.icon(
-                  onPressed: () => print('live'),
-                  icon: const Icon(
-                    Icons.videocam,
-                    color: Colors.red,
-                  ),
-                  label: Text('Live'),
+                ProfileAvatar(
+                  imageUrl: currentUser.imageUrl,
                 ),
-                const VerticalDivider(width: 8.0),
-                FlatButton.icon(
-                  onPressed: () => print('photo'),
-                  icon: const Icon(
-                    Icons.photo_library,
-                    color: Colors.green,
-                  ),
-                  label: Text('Photo'),
+                const SizedBox(
+                  width: 8.0,
                 ),
-                const VerticalDivider(width: 8.0),
-                FlatButton.icon(
-                  onPressed: () => print('room'),
-                  icon: const Icon(
-                    Icons.video_call,
-                    color: Colors.purpleAccent,
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'What\'s on your mind?',
+                    ),
                   ),
-                  label: Text('Room'),
                 ),
               ],
             ),
-          )
-        ],
+            const Divider(height: 10.0, thickness: .5),
+            Container(
+              height: 40.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FlatButton.icon(
+                    onPressed: () => print('live'),
+                    icon: const Icon(
+                      Icons.videocam,
+                      color: Colors.red,
+                    ),
+                    label: Text('Live'),
+                  ),
+                  const VerticalDivider(width: 8.0),
+                  FlatButton.icon(
+                    onPressed: () => print('photo'),
+                    icon: const Icon(
+                      Icons.photo_library,
+                      color: Colors.green,
+                    ),
+                    label: Text('Photo'),
+                  ),
+                  const VerticalDivider(width: 8.0),
+                  FlatButton.icon(
+                    onPressed: () => print('room'),
+                    icon: const Icon(
+                      Icons.video_call,
+                      color: Colors.purpleAccent,
+                    ),
+                    label: Text('Room'),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
